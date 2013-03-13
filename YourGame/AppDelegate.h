a4b356911f3d7e09b2136a7b14c0a4b13d2c3ac6
Ed Williams <excelltechmobile@gmail.com>
2013-03-13 17:01:04 -0500

    first commit

---
+++ YourGame/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  YourGame
+//
+//  Created by Edward Williams on 3/11/13.
+//  Copyright (c) 2013 Sample Organization. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
